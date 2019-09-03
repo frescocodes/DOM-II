176520dd092e105243af41dce8a4af471a34f2a4
Fresco Fresco <fresco@Frescos-MacBook-Pro.local>
2019-09-03 18:47:07 -0400

    starting event listeners, so far have made site really ugly, plan to make it even uglier

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+
+// variables
+const changeBg = document.querySelector('body');
+const changeNav = document.querySelector('header');
+const changeP = document.getElementsByTagName("p");
+const moveImgs = document.querySelectorAll('img').forEach(item => {
+    item.addEventListener('mouseenter', event => {
+        item.style.transform = "rotateX(30)";
+        item.style.transition = 'all 0.5s';
+    })
+})
+
+changeBg.onload = uglyColors();
+
+
+function uglyColors(element) {
+    console.log('page is loaded');
+    changeNav.style.backgroundColor = 'limegreen';
+    changeBg.style.backgroundColor = 'orange';
+    changeP[0].style.backgroundColor = 'purple';
+    changeP[1].style.backgroundColor = 'red';
+    changeP[2].style.backgroundColor = 'pink';
+    changeP[3].style.backgroundColor = 'yellow';
+    changeP[4].style.backgroundColor = 'brown';
+    changeP[5].style.backgroundColor = 'blue';
+    changeP[6].style.backgroundColor = 'gray';
+    changeP[7].style.backgroundColor = 'black';
+    changeP[8].style.backgroundColor = 'aqua';
+    changeP[9].style.backgroundColor = 'snow';
+}
