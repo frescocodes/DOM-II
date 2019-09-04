73ea9e5e035a91b191d37c50ffef0293a7b8f1e3
Fresco Fresco <fresco@Frescos-MacBook-Pro.local>
2019-09-03 21:32:43 -0400

    more eye cancer and more fun. 7 event listeners so far

--- js/index.js
+++ js/index.js
@@ -4,29 +4,13 @@
 const changeBg = document.querySelector('body');
 const changeNav = document.querySelector('header');
 const changeP = document.getElementsByTagName("p");
+const headings = document.getElementsByTagName('h1');
+
+let randColor = Math.floor(Math.random()*16777215).toString(16);
+let randColorStr = "'#" + randColor + "'";
+console.log(randColorStr);
 
 changeBg.onload = uglyColors();
-
-const moveImgs = document.querySelectorAll('img').forEach(item => {
-    item.addEventListener('mouseenter', event => {
-        item.style.transform = "rotate(9999999deg)";
-        // item.style.transform = "rotate(283487923deg)";
-        item.style.transition = 'all 180s';
-    })
-})
-
-const scaleNav = document.querySelectorAll('header a').forEach(item => {
-    item.addEventListener('mouseleave', event => {
-        item.style.transform = 'scale(5)';
-        item.style.transition = 'easein 1s';
-    })
-})
-
-const kevOreo = document.querySelector('body').addEventListener('dblclick', event => {
-    changeBg.style.backgroundImage = 'url(https://cdn.discordapp.com/emojis/613072162424619018.png?v=1)';
-})
-
-
 function uglyColors(element) {
     console.log('page is loaded');
     changeNav.style.backgroundColor = 'limegreen';
@@ -42,3 +26,46 @@ function uglyColors(element) {
     changeP[8].style.backgroundColor = 'aqua';
     changeP[9].style.backgroundColor = 'snow';
 }
+
+const moveImgs = document.querySelectorAll('img').forEach(item => {
+    item.addEventListener('mouseenter', event => {
+        item.style.transform = "rotate(9999999deg)";
+        item.style.transition = 'all 180s';
+    })
+})
+
+const scaleNav = document.querySelectorAll('header a').forEach(item => {
+    item.addEventListener('mouseleave', event => {
+        event.preventDefault();
+        item.style.transform = 'scale(5)';
+        item.style.transition = 'easein 1s';
+    })
+})
+
+const kevOreo = document.querySelector('body').addEventListener('dblclick', event => {
+    changeBg.style.backgroundImage = 'url(https://cdn.discordapp.com/emojis/613072162424619018.png?v=1)';
+    // changeBg.style.backgroundColor = randColorStr;
+});
+
+changeP.onmousedown = document.querySelectorAll('p').forEach(item => {
+    item.addEventListener('mousedown', event => {
+    item.style.backgroundColor = 'magenta';
+})
+});
+
+headings.oncontextmenu = document.querySelectorAll('body').forEach(item => {
+    item.addEventListener('contextmenu', event => {
+        alert('How dare you try to steal my code')
+        alert('Don\'t you ever try that again')
+        prompt('You promise?')
+        if (prompt == 'yes') {
+            return alert('thanks')
+        } else {
+            return prompt('you have to promise')
+        }
+    })
+})
+
+changeBg.onkeypress = document.querySelector('body').addEventListener('keypress', event => {
+    changeBg.style.transform = 'rotate(870deg)';
+})
